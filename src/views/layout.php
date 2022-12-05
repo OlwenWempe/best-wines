0548adffd46d269fde14b186677fdfda1820ef8d
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-05 15:25:31 +0100

    install parcel-bundler. Added .env.example

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,16 +1,19 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Best wines</title>
 </head>
+
 <body>
 
 
-        <?= $content ?>
+    <?= $content ?>
 
 </body>
+
 </html>
