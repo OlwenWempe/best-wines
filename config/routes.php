3fb4b22951795fcf09bea3fc11bf96a52a4c5070
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-06 09:22:42 +0100

    added some routes

--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,7 @@ use Core\Router;
 
 Router::register('/products/all', 'ProductController::index');
 Router::register('/login', 'UserController::login');
+Router::register('/logout', 'UserController::logout');
 Router::register('/register', 'UserController::register');
 Router::register('/product/delete', 'ProductController::delete');
 Router::register('/product/', 'ProductController::show');
--- src/views/product/index.php
+++ src/views/product/index.php
@@ -1,4 +1,4 @@
-<h1>Ceci est la page index task</h1>
+<h1>Ceci est la page index de nos produits</h1>
 <h2><?= $message ?></h2>
 <ul>
     <?php foreach ($products as $product) : ?>
