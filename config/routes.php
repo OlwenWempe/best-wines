5ebe758c2139253ac9bcd039645d6f84730de8fe
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-06 15:23:35 +0100

    minor changes and updates

--- config/routes.php
+++ config/routes.php
@@ -5,10 +5,31 @@ require_once 'core/Router.php';
 
 use Core\Router;
 
-Router::register('/products/all', 'ProductController::index');
+//client
+
 Router::register('/login', 'UserController::login');
 Router::register('/logout', 'UserController::logout');
 Router::register('/register', 'UserController::register');
+
+//admin
+
+Router::register('/admin/login', 'AdminController::login');
+Router::register('/admin/logout', 'AdminController::logout');
+Router::register('/admin/register', 'AdminController::register');
+
+//supplier
+
+Router::register('/supplier/login', 'SupplierController::login');
+Router::register('/supplier/logout', 'SupplierController::logout');
+Router::register('/supplier/register', 'SupplierController::register');
+
+//coffrets
+
+
+
+//nos vins
+
+Router::register('/products/all', 'ProductController::index');
 Router::register('/product/delete', 'ProductController::delete');
 Router::register('/product/', 'ProductController::show');
 Router::register('/product/add', 'ProductController::insert');
