31d7b9f0371cc1c5e2976abc99af7dd02f864dbf
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-07 14:16:52 +0100

    little upgrade

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -12,6 +12,10 @@ class UserController extends Controller
     public function login()
     {
         $this->renderView('user/login');
+        //verifier si dans la bdd admin
+        header("Location admin/login.php");
+        //sinon verification dans la BDD supplier
+        // sinon verification dans la bdd client
         // echo "ceci est la méthode login";
     }
 
