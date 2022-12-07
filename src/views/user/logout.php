cfdde4c28b8ff008794f0357837b478ffb35b767
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-07 14:17:23 +0100

    little changes

--- src/views/user/logout.php
+++ src/views/user/logout.php
@@ -1,11 +1,20 @@
 <?php
-  // Initialiser la session
-  session_start();
-  
-  // Détruire la session.
-  if(session_destroy())
-  {
-    // Redirection vers la page de connexion
-    header("Location: login.php");
-  }
+// Initialiser la session
+session_start();
+
+// Détruire la session.
+if (session_destroy()) {
+  // Redirection vers la page de connexion
+  header("Location: login.php");
+}
 ?>
+
+//la partie au-dessus est à mettre dans UserController dans la méthode logout. Faudra remplacer l'echo.
+//La partie en-dessous servira à mettre la mise en page du message logout plus un bouton "page d'acceuil".
+
+<section>
+
+
+
+
+</section>
