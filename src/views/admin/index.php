43a277cef3a3f462cca507b0db09683cd05a0afc
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-08 21:47:36 +0100

    added admin homepage ongoing

--- src/views/admin/index.php
+++ src/views/admin/index.php
@@ -1,9 +1,11 @@
 <section>
-    <h1>Bienvenue sur votre page admin</h1>
-    <aside>
-        <h2>Ajouter des produits</h2>
-        <hr>
-        <button class="btn btn-primary">Ajouter un vin</button>
-        <button class="btn btn-primary">Ajouter un box</button>
-    </aside>
+    <h1 class="text-center py-5">Bienvenue sur votre page admin</h1>
+    <div class="row">
+        <aside class="col-3 border-end">
+            <h2>Ajouter des produits</h2>
+            <hr class="ps-2">
+            <button class="btn btn-primary"><a href="">Ajouter un vin</a></button>
+            <button class="btn btn-primary">Ajouter un box</button>
+        </aside>
+    </div>
 </section>
