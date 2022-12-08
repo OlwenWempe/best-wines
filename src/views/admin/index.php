a8e5ee945eac7481d8ebadcd6d899256aeefd509
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-08 17:55:02 +0100

    start view homepage admin

---
+++ src/views/admin/index.php
@@ -0,0 +1,9 @@
+<section>
+    <h1>Bienvenue sur votre page admin</h1>
+    <aside>
+        <h2>Ajouter des produits</h2>
+        <hr>
+        <button class="btn btn-primary">Ajouter un vin</button>
+        <button class="btn btn-primary">Ajouter un box</button>
+    </aside>
+</section>
