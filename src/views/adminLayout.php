34ff71dd0ab5ea5624b3a41e7e56e2d9e104d811
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-11 00:42:50 +0100

    decorative changes

--- src/views/adminLayout.php
+++ src/views/adminLayout.php
@@ -41,55 +41,56 @@
         </nav>
     </header>
     <main>
-        <section class="row">
-            <aside class="col-3 border-end">
+        <section class="pb-5 row">
+            <aside class=" col-3 border-end">
                 <div class="container">
                     <h2>Afficher la liste des produits</h2>
                     <hr class="ps-2">
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/indexwine">Afficher les
-                            vins</a></button>
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/indexbox">Afficher les
-                            box</a></button>
+                    <a class="my-2 btn btn-primary" href="<?= BASE_DIR ?>/admin/indexwine">Afficher les
+                        vins</a>
+                    <a class="my-2 btn btn-primary" href="<?= BASE_DIR ?>/admin/indexbox">Afficher les
+                        box</a>
                 </div>
                 <div class="container">
                     <h2>Ajouter des produits</h2>
                     <hr class="ps-2">
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/addwine">Ajouter un
-                            vin</a></button>
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/addbox">Ajouter un
-                            box</a></button>
+                    <a class="my-2 btn btn-primary" href="<?= BASE_DIR ?>/admin/addwine">Ajouter un
+                        vin</a>
+                    <a class="my-2 btn btn-primary" href="<?= BASE_DIR ?>/admin/addbox">Ajouter un
+                        box</a>
                 </div>
                 <div class="container">
                     <h2>Ajouter des tags</h2>
                     <hr class="ps-2">
-                    <form action="<?= BASE_DIR ?>/admin/addregion" method="post">
-                        <label for="region">Ajouter une région</label>
-                        <input type="text" name="name" id="region">
-                        <select name="id_pays" id="id_pays">
+                    <form class="mt-3" action="<?= BASE_DIR ?>/admin/addregion" method="post">
+                        <label class="ms-2 form-label" for="region">Ajouter une région</label>
+                        <input class="form-control" type="text" name="name" id="region">
+                        <select class="form-control" name="id_pays" id="id_pays">
                             <option selected value="">Selectionner un pays</option>
                             <option value="75">France</option>
                         </select>
-                        <input type="submit" name="soumettre" value="soumettre">
+                        <input class="my-2 btn btn-primary form-control" type="submit" name="soumettre"
+                            value="soumettre">
                     </form>
-                    <form action="<?= BASE_DIR ?>/admin/addtype" method="post">
-                        <label for="type">Ajouter un type</label>
-                        <input type="text" name="name" id="type">
-                        <input type="submit" name="soumettre" value="soumettre">
+                    <form class="mt-3" action="<?= BASE_DIR ?>/admin/addtype" method="post">
+                        <label class="ms-2 form-label" for="type">Ajouter un type</label>
+                        <input class="form-control" type="text" name="name" id="type">
+                        <input class="my-2 btn btn-primary form-control" type="submit" name="soumettre"
+                            value="soumettre">
                     </form>
-                    <form action="<?= BASE_DIR ?>/admin/addtaste" method="post">
-                        <label for="taste">Ajouter un goût</label>
-                        <input type="text" name="name" id="taste">
-                        <input type="submit" name="soumettre" value="soumettre">
+                    <form class="mt-3" action="<?= BASE_DIR ?>/admin/addtaste" method="post">
+                        <label class="ms-2 form-label" for="taste">Ajouter un goût</label>
+                        <input class="form-control" type="text" name="name" id="taste">
+                        <input class="my-2 btn btn-primary form-control" type="submit" name="soumettre"
+                            value="soumettre">
                     </form>
-                    <form action="<?= BASE_DIR ?>/admin/addaccord" method="post">
-                        <label for="accord">Ajouter un accord</label>
-                        <input type="text" name="name" id="accord">
-                        <input type="submit" name="soumettre" value="soumettre">
+                    <form class="mt-3 mb-5" action="<?= BASE_DIR ?>/admin/addaccord" method="post">
+                        <label class="ms-2 form-label" for="accord">Ajouter un accord</label>
+                        <input class="form-control" type="text" name="name" id="accord">
+                        <input class="my-2 btn btn-primary form-control" type="submit" name="soumettre"
+                            value="soumettre">
                     </form>
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/addwine">Ajouter un
-                            vin</a></button>
-                    <button class="my-2 btn btn-primary"><a href="<?= BASE_DIR ?>/admin/addbox">Ajouter un
-                            box</a></button>
+
                 </div>
             </aside>
             <div class="col-9">
