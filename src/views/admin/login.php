e97f49f6fbd44e7b474e8bcd76e658d55bbe8bdf
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-10 14:32:28 +0100

    idem

--- src/views/admin/login.php
+++ src/views/admin/login.php
@@ -1,14 +1,13 @@
 <section class="mt-5">
     <div class="card mx-auto" style="width: 25vw;">
-        <div class="card-body">
+        <form method="POST" class="card-body">
             <div id="connection-card" class="card-header">
                 <h5 class="card-title text-center">Se connecter</h5>
             </div>
             <div class="mb-3 row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="staticEmail"
-                        value="email@example.com">
+                    <input type="text" readonly class="form-control-plaintext" id="adminEmail"
+                        placeholder="email@example.com">
                 </div>
             </div>
             <div class="mb-3 row">
@@ -17,7 +16,7 @@
                     <input type="password" class="form-control" id="inputPassword">
                 </div>
             </div>
-            <a href="#" class="btn btn-primary">Connexion</a>
-        </div>
+            <input type="submit" name="submit" class="btn btn-primary" value="Envoyer"></input>
+        </form>
     </div>
 </section>
