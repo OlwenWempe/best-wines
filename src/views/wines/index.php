4fc79f83d0e39f1696f1ed1db902fc16804824ab
RieSakura <rie.manzani@gmail.com>
2022-12-12 16:59:49 +0100

    update register
    Co-authored-by: goncalvesraphael <goncalvesraphael@users.noreply.github.com>
    Co-authored-by: Xavier <xavierdor@users.noreply.github.com>
    Co-authored-by: olwen.wempe@hotmail.fr <olwen.wempe@hotmail.fr>

--- src/views/wines/index.php
+++ src/views/wines/index.php
@@ -1,8 +1,13 @@
-<h1>Ceci est la page index de nos produits</h1>
-<ul>
-    <?php foreach ($wines as $wine) : ?>
 
-    <li><?= $wine['name'] ?></li>
 
-    <?php endforeach; ?>
-</ul>
+<?php foreach ($wines as $wine) : ?>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="uploadMini/<?= $wine['link_picture_mini'] ?>" alt="<?= $wine['name'] ?>">
+  <div class="card-body">
+    <h5 class="card-title"><?= $wine['name'] ?></h5>
+    <p class="card-text"><?= $wine['description'] ?></p>
+    <a href="href=<?= BASE_DIR ?>/nos-vins/id=<?= $wine['id'] ?>" class="btn btn-primary">Voir le vin</a>
+  </div>
+</div>
+<?php endforeach; ?>
+
