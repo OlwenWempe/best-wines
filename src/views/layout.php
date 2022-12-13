fced02b282a9fcef45f2cb46e9e4e07fe512b1eb
RieSakura <rie.manzani@gmail.com>
2022-12-13 11:32:16 +0100

    update layout Co-authored-by: goncalvesraphael <goncalvesraphael@users.noreply.github.com>

--- src/views/layout.php
+++ src/views/layout.php
@@ -88,9 +88,84 @@
 
     </main>
 
-    <footer>
+<footer>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_DIR ?>/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_DIR ?>/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_DIR ?>/favicon-16x16.png">
+    <link rel="manifest" href="<?= BASE_DIR ?>/site.webmanifest">
+    <link rel="stylesheet" href="<?= BASE_DIR ?>/assets/css/style.css">
+    <title><?= $params['title'] ?></title>
+</footer>
 
-    </footer>
+<body id="main-body">
+    <footer>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="<?= BASE_DIR ?>/assets/img/nav-logo.png"
+                        alt="brand logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Nos vins
+                            </a>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Nos rouges</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nos blanc</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nos rosés</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nos champagnes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nos mousseux</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nos coffrets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Aide</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mentions légales</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Presse</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Qui sommes nous ?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+                </footer>
+
     <script src="<?= BASE_DIR ?>/assets/js/index.js"></script>
     <script src="<?= BASE_DIR ?>/dist/index.js"></script>
     <!-- JavaScript Bundle with Popper -->
