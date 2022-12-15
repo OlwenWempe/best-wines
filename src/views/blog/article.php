c71b006ee27c982dc58aa2185f472c745e45fca7
RieSakura <rie.manzani@gmail.com>
2022-12-15 14:51:47 +0100

    update article

---
+++ src/views/blog/article.php
@@ -0,0 +1,33 @@
+<section class="my-5 row">
+<div class="col-8 container text-center">
+	<div class="row row-cols-1 row-cols-lg-4 g-2 g-lg-3 justify-content-around">
+<div class="card mx-2">
+<img class="card-img-top" src="<?= BASE_DIR ?>/assets/img/wine130.png" alt="brand logo">
+  <div class="card-body">
+    <h5 class="card-title">MAISON : UNE SÉRIE DE NATURES, MORTES, OCT. 2025</h5>
+    <p class="card-text">Trouver le caractère poignant de la banalité pendant la pandémie.</p>
+    <a href="#" class="btn btn-primary">Voir article</a>
+  </div>
+</div>
+
+<div class="card mx-2">
+<img class="card-img-top" src="<?= BASE_DIR ?>/assets/img/wine101.png" alt="brand logo">
+  <div class="card-body">
+    <h5 class="card-title">MAISON : UNE SÉRIE DE NATURES, MORTES, OCT. 2025</h5>
+    <p class="card-text">Trouver le caractère poignant de la banalité pendant la pandémie.</p>
+    <a href="#" class="btn btn-primary">Voir article</a>
+  </div>
+</div>
+
+<div class="card mx-2">
+<img class="card-img-top" src="<?= BASE_DIR ?>/assets/img/wine117.png" alt="brand logo">
+  <div class="card-body">
+    <h5 class="card-title">PORTRAITS POUR LA PAIX, GALERIE FOURNIER, FÉV. 2026</h5>
+    <p class="card-text">Journaliste du Figaro</p>
+    <a href="#" class="btn btn-primary">Voir article</a>
+  </div>
+</div>
+			</div>
+			</div>
+			</div>
+</section>
