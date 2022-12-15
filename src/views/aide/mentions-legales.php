943f00d684ac20361fc0cfd57c27277ec0f6155c
Olwen WEMPE <wempe.olwen@gmail.com>
2022-12-15 09:39:48 +0100

    small changes

--- src/views/aide/mentions-legales.php
+++ src/views/aide/mentions-legales.php
@@ -1,43 +1,72 @@
-<section class="my-5">
-<h1>Conditions d’utilisation :</h1>
-<p>
-Ce site (www.bestwines.fr) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort d’utilisation et un graphisme plus agréable.
-Nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
-L’agence web Best Wines met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet.
-Toutefois, des erreurs ou omissions peuvent survenir. L’internaute devra donc s’assurer de l’exactitude des informations auprès de Best Wine , et signaler toutes modifications du site qu’il jugerait utile. Best Wines n’est en aucun cas
-responsable de l’utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
-</p>
-<p>
-<h1>Cookies :</h1>
-Le site www.bestwines.fr peut-être amené à vous demander l’acceptation des cookies pour des besoins de statistiques et d’affichage. Un cookie est une information déposée sur votre disque dur par le serveur du site que vous
-visitez.
-Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple fichier texte auquel un serveur accède pour lire et enregistrer des informations . Certaines parties de ce site ne peuvent être fonctionnelles sans
-l’acceptation de cookies.
-Liens hypertextes : Les sites internet de peuvent offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet. Best Wines ne dispose d’aucun moyen pour contrôler les sites en connexion avec ses sites
-internet.
-Best Wines ne répond pas de la disponibilité de tels sites et sources externes, ni ne la garantit. Elle ne peut être tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources
-externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent pleinement à l’internaute, qui doit se conformer à leurs
-conditions d’utilisation.
-Les utilisateurs, les abonnés et les visiteurs des sites internet ne peuvent pas mettre en place un hyperlien en direction de ce site sans l’autorisation expresse et préalable de Best Wines.
-Dans l’hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des sites internet de Best Wines, il lui appartiendra d’adresser un email accessible sur le site afin de formuler sa demande de
-mise en place d’un hyperlien.
-</p>
-<p>
-<h1>CLimitation contractuelles sur les données :</h1>
-Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions.
-Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par courriel, à l’adresse contact@anthedesign.fr, en décrivant le problème de la manière la plus précise possible (page
-posant problème, type d’ordinateur et de navigateur utilisé, …).
-Tout contenu téléchargé se fait aux risques et périls de l’utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d’un quelconque dommage subi par l’ordinateur de l’utilisateur ou d’une quelconque
-perte de données consécutives au téléchargement.
-De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.
-Les liens hypertextes mis en place dans le cadre du présent site internet en direction d’autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de Best Wines.
-</p>
-<p>
-<h1>Propriété intellectuelle :</h1>
-Tout le contenu du présent site www.anthedesign.fr, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à
-l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs.
-Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l’accord exprès par écrit de Best Wines. Cette représentation ou
-reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant
-engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre
-</p>
+<section class="my-5 container">
+    <h1>Conditions d'utilisation :</h1>
+    <p>
+        Ce site (www.bestwines.fr) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un
+        meilleur confort d'utilisation et un graphisme plus agréable.
+        Nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google
+        Chrome, etc…
+        L'agence web Best Wines met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et
+        une mise à jour fiable de ses sites internet.
+        Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des
+        informations auprès de Best Wine , et signaler toutes modifications du site qu'il jugerait utile. Best Wines
+        n'est en aucun cas
+        responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en
+        découler.
+    </p>
+    <p>
+    <h1>Cookies :</h1>
+    Le site www.bestwines.fr peut-être amené à vous demander l'acceptation des cookies pour des besoins de statistiques
+    et d'affichage. Un cookie est une information déposée sur votre disque dur par le serveur du site que vous
+    visitez.
+    Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple fichier texte auquel un serveur
+    accède pour lire et enregistrer des informations . Certaines parties de ce site ne peuvent être fonctionnelles sans
+    l'acceptation de cookies.
+    Liens hypertextes : Les sites internet de peuvent offrir des liens vers d'autres sites internet ou d'autres
+    ressources disponibles sur Internet. Best Wines ne dispose d'aucun moyen pour contrôler les sites en connexion avec
+    ses sites
+    internet.
+    Best Wines ne répond pas de la disponibilité de tels sites et sources externes, ni ne la garantit. Elle ne peut être
+    tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources
+    externes, et notamment des informations, produits ou services qu'ils proposent, ou de tout usage qui peut être fait
+    de ces éléments. Les risques liés à cette utilisation incombent pleinement à l'internaute, qui doit se conformer à
+    leurs
+    conditions d'utilisation.
+    Les utilisateurs, les abonnés et les visiteurs des sites internet ne peuvent pas mettre en place un hyperlien en
+    direction de ce site sans l'autorisation expresse et préalable de Best Wines.
+    Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d'un des sites
+    internet de Best Wines, il lui appartiendra d'adresser un email accessible sur le site afin de formuler sa demande
+    de
+    mise en place d'un hyperlien.
+    </p>
+    <p>
+    <h1>CLimitation contractuelles sur les données :</h1>
+    Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes
+    périodes de l'année, mais peut toutefois contenir des inexactitudes ou des omissions.
+    Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler
+    par courriel, à l'adresse contact@anthedesign.fr, en décrivant le problème de la manière la plus précise possible
+    (page
+    posant problème, type d'ordinateur et de navigateur utilisé, …).
+    Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En
+    conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou
+    d'une quelconque
+    perte de données consécutives au téléchargement.
+    De plus, l'utilisateur du site s'engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus
+    et avec un navigateur de dernière génération mis-à-jour.
+    Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes
+    sur le réseau Internet ne sauraient engager la responsabilité de Best Wines.
+    </p>
+    <p>
+    <h1>Propriété intellectuelle :</h1>
+    Tout le contenu du présent site www.anthedesign.fr, incluant, de façon non limitative, les graphismes, images,
+    textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de
+    la société à
+    l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
+    Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces
+    différents éléments est strictement interdite sans l'accord exprès par écrit de Best Wines. Cette représentation ou
+    reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et
+    suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon
+    pouvant
+    engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés
+    pourraient intenter une action en justice à votre encontre
+    </p>
 </section>
