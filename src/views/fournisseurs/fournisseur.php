7b57d1e1495a77afa7529149347c842f10fe0736
RieSakura <rie.manzani@gmail.com>
2022-12-15 15:03:25 +0100

    git pull

---
+++ src/views/fournisseurs/fournisseur.php
@@ -0,0 +1,33 @@
+<section class="my-5">
+<h1>Nos fornisseurs</h1>
+			<div>
+					<div class="qui">
+						<h2>Terre, mer, air</h2>
+						<p>Journée portes ouvertes</p>
+                        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="<?= BASE_DIR ?>/assets/img/wine24.png"
+                        alt="brand logo"></a>
+					</div>
+</div>
+            <div>
+					<div class="qui">
+						<h2>Salle de fitness Colombes</h2>
+						<p>Les Olympiques de Condorcet</p>
+                        <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="<?= BASE_DIR ?>/assets/img/wine9.png"
+                        alt="brand logo"></a>
+					</div>
+					</div>
+			</div>
+            <div>
+					<div class="qui">
+						<h2>Librairie Descartes</h2>
+						<p>Fête de lancement</p>
+                        <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="<?= BASE_DIR ?>/assets/img/wine69.png"
+                        alt="brand logo"></a>
+					</div>
+					</div>
+			</div>
+</section>
