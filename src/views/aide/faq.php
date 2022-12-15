3e1075e84fdaac2bb87fb7ee443666d237cb9f04
RieSakura <rie.manzani@gmail.com>
2022-12-15 14:42:50 +0100

    update faq

--- src/views/aide/faq.php
+++ src/views/aide/faq.php
@@ -1,6 +1,10 @@
 <section class="my-5" id="bodyfaq">
 			
             <h1>FAQ</h1>
+			<div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="<?= BASE_DIR ?>/assets/img/wine14.png"
+                        alt="brand logo"></a>
+					</div>
 			<div>
 					<div class="faq">
 						<h2>Qu'est-ce que la page FAQ ?</h2>
